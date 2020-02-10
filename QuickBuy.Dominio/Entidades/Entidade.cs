5ca830e80426b53e485e7df0c0bf7be6c0121493
Guilherme Araujo <guilherme@autodoc.com.br>
2020-02-10 13:38:08 -0300

    Criação do repositorio

--- QuickBuy.Dominio/Entidades/Entidade.cs
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
     public abstract class Entidade
     {
+        public List<string> _mensagensValidacao { get; set; }
+
+        public List<string> MensagemValidacao
+        {
+            get { return _mensagensValidacao ?? (_mensagensValidacao = new List<string>()); }
+        }
+
+        protected void LimparMensagensValidacao()
+        {
+            MensagemValidacao.Clear();
+        }
+
+        public abstract void Validate();
+
+        protected bool EhValido
+        {
+            get { return !MensagemValidacao.Any(); }
+        }
     }
 }
--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,10 +1,11 @@
 ﻿using QuickBuy.Dominio.Entidades.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido: Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
@@ -19,5 +20,12 @@ namespace QuickBuy.Dominio.Entidades
         public FormaPagamento FormaPagamento { get; set; }
 
         public ICollection<ItemPedido> ItensPedido { get; set; }
+
+        public override void Validate()
+        {
+            LimparMensagensValidacao();
+            if (!ItensPedido.Any())
+                MensagemValidacao.Add("Pedido não pode ficar sem item de pedido");
+        }
     }
 }
